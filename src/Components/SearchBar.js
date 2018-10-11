e838e028ae9b26d34876cf52e98ae0426013facf
prokawsar <prokawsar@gmail.com>
2018-10-11 20:53:37 +0600

    download working and icon added

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -39,7 +39,7 @@ export default class SearchBar extends Component {
             <div>
                 <Input label="Type any Github Username" onChange={this.handleChange} group type="text" />
                 {/* <Input label="Type your password" icon="lock" group type="password" /> */}
-                <Button color="danger" onClick={this.handleSubmit} >Search </Button>
+                <Button color="danger" onClick={this.handleSubmit} >Search <Fa icon="search" /></Button>
                 <PanelBoard 
                     data={this.state.reposData}
                 />
@@ -61,6 +61,7 @@ class PanelBoard extends Component {
               name={repo.name}
               description={repo.description}
               lang={repo.language}
+              forks={repo.forks_count}
               clone_url={repo.clone_url}
               branch={repo.default_branch}
               dLink={repo.html_url}
@@ -86,18 +87,22 @@ class ResultPanel extends Component {
             <div>
                 <Card>
                     <CardBody>
-                        <CardTitle>{this.props.name}  
+                        <CardTitle>
+                            <a 
+                            title="View on Github" 
+                            href={this.props.dLink} target="_blank"> {this.props.name} <span><Fa icon="github" title="View on Github"/></span>
+                            </a>  
                             {/* <Button color="primary pull-right">Copy clone link</Button>  */}
-                            <span className="pull-right" ><Fa title="Download as zip" icon="download" href={this.props.dLink + this.state.url + this.props.branch + this.state.zip } /></span>
+                            {/* <span className="pull-right" ><Fa title="Download as zip" icon="download" href={this.props.dLink + this.state.url + this.props.branch + this.state.zip } /></span> */}
                             
                             <span className="pull-left" ><Fa title="Copy Clone link" icon="file" href={this.props.clone_url } /></span>
 
-                            {/* <Button color="primary pull-right" href={this.props.dLink + this.state.url + this.props.branch + this.state.zip }>Download as zip</Button>  */}
+                            <Button title="Download as zip" size="sm" color="primary pull-right" href={this.props.dLink + this.state.url + this.props.branch + this.state.zip }><Fa title="Download as zip" icon="download" /></Button> 
                             
                         </CardTitle>
                         <CardText>{this.props.description}</CardText>
-                        <span className="pull-right"><Fa icon="github" title="View on Github"/></span>
-                        <span className="pull-left">Language: {this.props.lang} </span>
+                        <CardText><span className="pull-left">Language: {this.props.lang} </span></CardText>
+                        <CardText><span className="pull-right">Forks: {this.props.forks} </span></CardText>
                        
                     </CardBody>
                 </Card>
